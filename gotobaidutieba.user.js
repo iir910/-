64a11347617b17c38ebcead663f20db058536d96
iir910 <lpq910@gmail.com>
2018-11-11 21:09:28 +0800

    Update gotobaidutieba.user.js

--- gotobaidutieba.user.js
+++ gotobaidutieba.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           百度贴吧-重定向
-// @version        1.0
+// @version        1.01
 // @author        
 // @description    重定向各类同素异形体到更科学的主域名tieba.baidu.com
 // @namespace     
@@ -8,7 +8,7 @@
 // @include        /.\.tieba\.baidu\.com/
 // @include        /post\.baidu(\.com)?(\.cn)?/
 // @include        /jump\.bdimg\.com\/(?!safecheck\/)/
-// @include        /dq.tieba.com/*
+// @include         http*://dq.tieba.com/*
 //////
 // @run-at         document-start
 // ==/UserScript==
