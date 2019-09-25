b6e99787a288d5988381fd5f372605a59e53bc67
agunico <lpq910@gmail.com>
2019-09-25 19:36:21 +0800

    Update gotobaidutieba.user.js

--- gotobaidutieba.user.js
+++ gotobaidutieba.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name           百度贴吧-重定向
-// @version        1.01
+// @name           百度贴吧-域名重定向
+// @version        1.02
 // @author        
-// @description    重定向各类同素异形体到更科学的主域名tieba.baidu.com
+// @description    重定向各类同素异形体到更科学的主域名tieba.baidu.com （修改自用版本）
 // @namespace     
 // @include        /tieba\.baidu(\.com)?\.cn/
 // @include        /.\.tieba\.baidu\.com/
